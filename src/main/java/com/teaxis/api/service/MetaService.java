f58cdab58c9b70c6e144191c15655d5dfd292b51
Eduarda <eduardaveloso@outlook.com.br>
2025-05-08 14:40:12 -0300

    ultima atualização no metaservice

--- src/main/java/com/teaxis/api/service/MetaService.java
+++ src/main/java/com/teaxis/api/service/MetaService.java
@@ -3,6 +3,7 @@ package com.teaxis.api.service;
 import com.teaxis.api.model.Meta;
 import com.teaxis.api.model.Usuario;
 import com.teaxis.api.repository.MetaRepository;
+import com.teaxis.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,17 +15,20 @@ public class MetaService {
     @Autowired
     private MetaRepository repository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     public List<Meta> listar() {
         return repository.findAll();
     }
 
     public Meta salvar(Meta meta) {
-
+        
         Long usuarioId = meta.getUsuario().getId();
         Usuario usuarioCompleto = usuarioRepository.findById(usuarioId)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + usuarioId));
 
-        meta.setUsuario(usuarioCompleto);
+        meta.setUsuario(usuarioCompleto); 
         return repository.save(meta);
     }
 
@@ -50,11 +54,14 @@ public class MetaService {
                     metaExistente.setProgresso(metaAtualizada.getProgresso());
                 }
                 if (metaAtualizada.getUsuario() != null) {
-                    metaExistente.setUsuario(metaAtualizada.getUsuario());
+                    
+                    Long usuarioId = metaAtualizada.getUsuario().getId();
+                    Usuario usuarioCompleto = usuarioRepository.findById(usuarioId)
+                            .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + usuarioId));
+                    metaExistente.setUsuario(usuarioCompleto);
                 }
                 return repository.save(metaExistente);
             })
             .orElseThrow(() -> new RuntimeException("Meta não encontrada com o ID: " + id));
     }
-    
 }
